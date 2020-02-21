18614baa70e38ee3d9e5f70ffbcc96071546c163
RamanathanVaradharajan <ramanathan.varadharajan@wur.nl>
2020-02-21 23:15:27 +0100

    Documentation of main module

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
+"""
+Main module.
+
+This program attempts to predict the price of a car using machine learning.
+Data of used cars along with its attributes like speed, mileage are provided
+as a training set. Multiple linear regression is used over chosen primary
+components that directly correlates with the price of the car. Model is later
+used to predic the price of test set. Model is evaluated and cross validated.
+"""
 from car_features import file_operations
 
 
